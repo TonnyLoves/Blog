786150b165e2312bc15125e915d0bd1d1ff82f2b
tangqiang <TG_IT_2011@163.com>
2022-06-15 20:08:02 +0800

    feat(redux): 新增redux相关知识

--- demo/深入理解Redux/src/redux-aswmoe/src/App.js
+++ demo/深入理解Redux/src/redux-aswmoe/src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { useStore } from 'react-redux';
+import { useDispatch, useSelector, useStore } from 'react-redux';
 
 function App(props) {
-
-  const { store } = useStore({counter: 1})
+  const dispatch = useDispatch()
+  const counter = useSelector((state) => {
+    return state.counter
+  })
   return (
-    <div className="App" style={{ 
-      backgroundColor: "red",
-      height: '100px',
-      display: 'block'
-    }}>
-      <text>{"测试"}</text>
+    <div 
+      className="App" 
+      style={{ 
+        backgroundColor: "red",
+        height: '100px',
+        display: 'block',
+      }}
+      onClick={() => dispatch({type: "add"})}
+    >
+      <div>{counter}</div>
     </div>
   );
 }
